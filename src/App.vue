92dbef5373795ce653948322463e606be7394822
raul gustavo <raul.animations@gmail.com>
2017-07-29 11:35:21 -0500

    Archivos iniciales

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+      <h1>PlatziMusic</h1>
+      <select v-model="selectedCountry">
+        <option v-for="country in countries" v-bind:values="country.value">{{ country.name }}</option>
+      </select>
+      <spinner v-show="loading"></spinner>
+      <ul>
+        <artist v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid"></artist>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+import getArtists from './api'
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+      { name:'Argentina', value:'argentina'},
+      { name:'Espania', value:'spain'},
+      { name:'Mexico', value:'mexico'}
+      ],
+      selectedCountry : 'argentina',
+      loading: true
+    }
+  },
+  methods:{
+    refreshArtist(){
+      const self = this
+      this.loading = true
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    },
+
+  },
+  mounted(){
+    this.refreshArtist();
+
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtist();
+    }
+  }
+}
+</script>
+
+<style lang='stylus' >
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+//Application name	platzimusic
+//API key	2d71049f1f6281e5d72b29bda48d19d2
+//Shared secret	c3abd7db3fc8457933302f62e83104bf
+//Registered to	programerguy
+
+export default {
+	appName: 'platzimusic',
+	apiKey: '2d71049f1f6281e5d72b29bda48d19d2',
+	secret: 'c3abd7db3fc8457933302f62e83104bf',
+	registeredTo: 'programerguy'
+}
